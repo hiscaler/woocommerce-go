3f9f566199eadf23978b7fd1993c8cc19696f20f
hiscaler <hiscaler@gmail.com>
2022-05-18 20:08:16 +0800

    Fix: Clean unused files

---
+++ entity/setting_group.go
@@ -0,0 +1,10 @@
+package entity
+
+// SettingGroup setting group properties
+type SettingGroup struct {
+	ID          string `json:"id"`
+	Label       string `json:"label"`
+	Description string `json:"description"`
+	ParentId    string `json:"parent_id"`
+	SubGroups   string `json:"sub_groups"`
+}
--- setting.go
+++ setting.go
@@ -1,18 +1,13 @@
 package woocommerce
 
-import jsoniter "github.com/json-iterator/go"
+import (
+	"github.com/hiscaler/woocommerce-go/entity"
+	jsoniter "github.com/json-iterator/go"
+)
 
 type settingService service
 
-type Group struct {
-	ID          string `json:"id"`
-	Label       string `json:"label"`
-	Description string `json:"description"`
-	ParentId    string `json:"parent_id"`
-	SubGroups   string `json:"sub_groups"`
-}
-
-func (s settingService) All() (items []Group, err error) {
+func (s settingService) All() (items []entity.SettingGroup, err error) {
 	resp, err := s.httpClient.R().Get("/settings")
 	if err != nil {
 		return
