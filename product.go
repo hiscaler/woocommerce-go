ff921859e7ca56a1beb80a842a4707c7e9f5c56a
hiscaler <hiscaler@gmail.com>
2022-05-19 10:01:37 +0800

    Add: add product create,update,delete method

--- product.go
+++ product.go
@@ -83,3 +83,115 @@ func (s productService) One(id int) (item entity.Product, err error) {
 	}
 	return
 }
+
+// Create
+
+type CreateProductRequest struct {
+	Name              string                           `json:"name,omitempty"`
+	Slug              string                           `json:"slug,omitempty"`
+	Type              string                           `json:"type,omitempty"`
+	Status            string                           `json:"status,omitempty"`
+	Featured          bool                             `json:"featured,omitempty"`
+	CatalogVisibility string                           `json:"catalog_visibility,omitempty"`
+	Description       string                           `json:"description,omitempty"`
+	ShortDescription  string                           `json:"short_description,omitempty"`
+	SKU               string                           `json:"sku,omitempty"`
+	RegularPrice      string                           `json:"regular_price,omitempty"`
+	SalePrice         string                           `json:"sale_price,omitempty"`
+	DateOnSaleFrom    string                           `json:"date_on_sale_from,omitempty"`
+	DateOnSaleFromGMT string                           `json:"date_on_sale_from_gmt,omitempty"`
+	DateOnSaleTo      string                           `json:"date_on_sale_to,omitempty"`
+	DateOnSaleToGMT   string                           `json:"date_on_sale_to_gmt,omitempty"`
+	Virtual           bool                             `json:"virtual,omitempty"`
+	Downloadable      bool                             `json:"downloadable,omitempty"`
+	Downloads         []entity.ProductDownload         `json:"downloads,omitempty"`
+	DownloadLimit     int                              `json:"download_limit,omitempty"`
+	DownloadExpiry    int                              `json:"download_expiry,omitempty"`
+	ExternalUrl       string                           `json:"external_url,omitempty"`
+	ButtonText        string                           `json:"button_text,omitempty"`
+	TaxStatus         string                           `json:"tax_status,omitempty"`
+	TaxClass          string                           `json:"tax_class,omitempty"`
+	ManageStock       bool                             `json:"manage_stock,omitempty"`
+	StockQuantity     int                              `json:"stock_quantity,omitempty"`
+	StockStatus       string                           `json:"stock_status,omitempty"`
+	Backorders        string                           `json:"backorders,omitempty"`
+	SoldIndividually  bool                             `json:"sold_individually,omitempty"`
+	Weight            string                           `json:"weight,omitempty"`
+	Dimensions        *entity.ProductDimension         `json:"dimensions,omitempty"`
+	ShippingClass     string                           `json:"shipping_class,omitempty"`
+	ReviewsAllowed    bool                             `json:"reviews_allowed,omitempty"`
+	UpsellIds         []int                            `json:"upsell_ids,omitempty"`
+	CrossSellIds      []int                            `json:"cross_sell_ids,omitempty"`
+	ParentId          int                              `json:"parent_id,omitempty"`
+	PurchaseNote      string                           `json:"purchase_note,omitempty"`
+	Categories        []entity.ProductCategory         `json:"categories,omitempty"`
+	Tags              []entity.ProductTag              `json:"tags,omitempty"`
+	Images            []entity.ProductImage            `json:"images,omitempty"`
+	Attributes        []entity.ProductAttribute        `json:"attributes,omitempty"`
+	DefaultAttributes []entity.ProductDefaultAttribute `json:"default_attributes,omitempty"`
+	GroupedProducts   []int                            `json:"grouped_products,omitempty"`
+	MenuOrder         int                              `json:"menu_order,omitempty"`
+	MetaData          []entity.Meta                    `json:"meta_data,omitempty"`
+}
+
+func (m CreateProductRequest) Validate() error {
+	return validation.ValidateStruct(&m,
+		validation.Field(&m.Name, validation.Required.Error("商品名称不能为空")),
+		validation.Field(&m.Type, validation.When(m.Type != "", validation.In("simple", "grouped", "external ", "variable").Error("无效的类型"))),
+		validation.Field(&m.Status, validation.When(m.Status != "", validation.In("draft", "pending", "private", "publish").Error("无效的状态"))),
+		validation.Field(&m.CatalogVisibility, validation.When(m.CatalogVisibility != "", validation.In("visible", "catalog", "search", "hidden").Error("无效的目录可见性"))),
+		validation.Field(&m.TaxStatus, validation.When(m.TaxStatus != "", validation.In("taxable", "shipping ", "none").Error("无效的税务状态"))),
+		validation.Field(&m.StockStatus, validation.When(m.StockStatus != "", validation.In("instock", "outofstock ", "onbackorder").Error("无效的库存状态"))),
+		validation.Field(&m.Backorders, validation.When(m.Backorders != "", validation.In("yes", "no ", "notify").Error("无效的缺货订单状态"))),
+	)
+}
+
+// Create create a product
+func (s productService) Create(req CreateProductRequest) (item entity.Product, err error) {
+	if err = req.Validate(); err != nil {
+		return
+	}
+
+	resp, err := s.httpClient.R().SetBody(req).Post("/products")
+	if err != nil {
+		return
+	}
+
+	if resp.IsSuccess() {
+		err = jsoniter.Unmarshal(resp.Body(), &item)
+	}
+	return
+}
+
+// Update
+
+type UpdateProductRequest = CreateProductRequest
+
+func (s productService) Update(id int, req UpdateProductRequest) (item entity.Product, err error) {
+	if err = req.Validate(); err != nil {
+		return
+	}
+
+	resp, err := s.httpClient.R().SetBody(req).Put(fmt.Sprintf("/products/%d", id))
+	if err != nil {
+		return
+	}
+
+	if resp.IsSuccess() {
+		err = jsoniter.Unmarshal(resp.Body(), &item)
+	}
+	return
+}
+
+// Delete delete a product
+func (s productService) Delete(id int, force bool) (item entity.Product, err error) {
+	resp, err := s.httpClient.R().SetBody(map[string]bool{"force": force}).Delete(fmt.Sprintf("/products/%d", id))
+	if err != nil {
+		return
+	}
+
+	if resp.IsSuccess() {
+		err = jsoniter.Unmarshal(resp.Body(), &item)
+	}
+	return
+}
--- product_test.go
+++ product_test.go
@@ -1,25 +1,66 @@
 package woocommerce
 
 import (
-	"github.com/hiscaler/gox/jsonx"
+	"errors"
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestProductService_All(t *testing.T) {
 	params := ProductsQueryParams{}
-	products, _, err := wooClient.Services.Product.All(params)
+	items, _, err := wooClient.Services.Product.All(params)
 	if err != nil {
 		t.Errorf("wooClient.Services.Product.All: %s", err.Error())
 	} else {
-		t.Logf("Products: %s", jsonx.ToPrettyJson(products))
+		if len(items) > 0 {
+			mainId = items[0].ID
+		}
 	}
 }
 
 func TestProductService_One(t *testing.T) {
-	product, err := wooClient.Services.Product.One(625)
+	t.Run("TestProductService_All", TestProductService_All)
+	product, err := wooClient.Services.Product.One(mainId)
 	if err != nil {
 		t.Errorf("wooClient.Services.Product.One: %s", err.Error())
 	} else {
-		t.Logf("Product: %s", jsonx.ToPrettyJson(product))
+		assert.Equal(t, mainId, product.ID, "product id")
+	}
+}
+
+func TestProductService_CreateUpdateDelete(t *testing.T) {
+	name := gofakeit.Word()
+	req := CreateProductRequest{
+		Name: name,
+	}
+	item, err := wooClient.Services.Product.Create(req)
+	if err != nil {
+		t.Fatalf("wooClient.Services.Product.Create error: %s", err.Error())
+	}
+	productId := item.ID
+	assert.Equal(t, name, item.Name, "product name")
+	name = gofakeit.Word()
+	updateReq := UpdateProductRequest{
+		Name: name,
+	}
+	_, err = wooClient.Services.Product.Update(productId, updateReq)
+	if err != nil {
+		t.Fatalf("wooClient.Services.Product.Update error: %s", err.Error())
+	}
+	item, err = wooClient.Services.Product.One(productId)
+	if err != nil {
+		t.Fatalf("wooClient.Services.Product.One error: %s", err.Error())
+	}
+	assert.Equal(t, name, item.Name, "product name")
+
+	// Delete
+	_, err = wooClient.Services.Product.Delete(productId, true)
+	if err != nil {
+		t.Fatalf("wooClient.Services.Product.Delete error: %s", err.Error())
+	}
+	_, err = wooClient.Services.Product.One(productId)
+	if !errors.Is(err, ErrNotFound) {
+		t.Fatalf("wooClient.Services.Product.Delete(%d) failed", productId)
 	}
 }
