bb326f76daba25b45eca05065da300bda4a94469
ViktorLi <butekl@inbox.lv>
2024-11-02 11:05:41 +0200

    Save QueryParams set by user otherwise they were rewritten with empty params

--- woo.go
+++ woo.go
@@ -1,6 +1,7 @@
 // Package woocommerce is a Woo Commerce lib.
 //
 // Quick start:
+//
 //	b, err := os.ReadFile("./config/config_test.json")
 //	if err != nil {
 //	   panic(fmt.Sprintf("Read config error: %s", err.Error()))
@@ -30,12 +31,6 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
-	"github.com/go-resty/resty/v2"
-	"github.com/hiscaler/gox/inx"
-	"github.com/hiscaler/gox/stringx"
-	"github.com/hiscaler/woocommerce-go/config"
-	jsoniter "github.com/json-iterator/go"
-	"github.com/json-iterator/go/extra"
 	"log"
 	"net"
 	"net/http"
@@ -45,6 +40,13 @@ import (
 	"strings"
 	"time"
 	"unsafe"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/hiscaler/gox/inx"
+	"github.com/hiscaler/gox/stringx"
+	"github.com/hiscaler/woocommerce-go/config"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/json-iterator/go/extra"
 )
 
 const (
@@ -171,7 +173,7 @@ func NewClient(config config.Config) *WooCommerce {
 			}).DialContext,
 		}).
 		OnBeforeRequest(func(client *resty.Client, request *resty.Request) error {
-			params := url.Values{}
+			params := request.QueryParam //Save query params set by user
 			for k, vs := range request.QueryParam {
 				var v string
 				switch len(vs) {
