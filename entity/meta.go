36b3bc062b8d9d79a809f0dd941a501e31c7eff8
Erfan Diakoo <diakoo123@gmail.com>
2025-05-25 14:55:30 +0330

    Enh: Implement custom UnmarshalJSON method for Meta struct to handle json.RawMessage

--- entity/meta.go
+++ entity/meta.go
@@ -1,7 +1,32 @@
 package entity
 
+import "encoding/json"
+
 type Meta struct {
 	ID    int    `json:"id"`
 	Key   string `json:"key"`
 	Value string `json:"value"`
+}
+
+func (m *Meta) UnmarshalJSON(data []byte) error {
+	type Alias Meta
+	aux := &struct {
+		Value json.RawMessage `json:"value"`
+		*Alias
+	}{
+		Alias: (*Alias)(m),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	var str string
+	if err := json.Unmarshal(aux.Value, &str); err == nil {
+		m.Value = str
+		return nil
+	}
+
+	m.Value = string(aux.Value)
+	return nil
 }
